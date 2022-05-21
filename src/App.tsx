9988cb64260f917e5c73cfd8ce9c9f82e61280f0
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-21 16:10:13 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+          <Header />
+          <Technologies/>
+      </div>
   );
+}
+
+function Technologies() {
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    );
 }
 
 export default App;
