0393995ef6c55a1f15b8921b9c7a3f1a8e9d5e97
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-26 17:02:43 +0300

    14 css-modules

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 export const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.profile}>
             <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
@@ -17,10 +18,10 @@ export const Profile = () => {
                 </div>
             </div>
             <div>
-                <div>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
