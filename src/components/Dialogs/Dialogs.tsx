31f6808b8de3cc1613fef443535c2be26f572ea9
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-06-01 17:57:07 +0300

    21. added layout for Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,6 +4,18 @@ import s from "./Dialogs.module.css"
 export const Dialogs = () => {
 
     return(
-        <div>dialogs</div>
+        <div className={s.dialogsWrapper}>
+            <div className={s.dialogs}>
+                <div className={s.dialog+" "+s.active}>Dima</div>
+                <div className={s.dialog}>Valera</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Sveta</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>hi!</div>
+                <div className={s.message}>How are you&</div>
+            </div>
+        </div>
     );
 }
