1ad18cef1d2d9364b1fdaa0c92930fdb7e2c6363
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-06-09 17:33:47 +0300

    25 map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,20 +3,54 @@ import s from "./Dialogs.module.css"
 import {Dialog} from "./Dialog";
 import {Message} from "./Message";
 
+
+type FriendType = {
+    name: string
+    id: number
+}
+type DialogsDatatype = FriendType[]
+
+type MessageType = {
+    message: string
+    id: number
+}
+type MessagesDatatype = MessageType[]
+
 export const Dialogs = () => {
+
+    const DialogsData: DialogsDatatype = [
+        {name: "Dima", id: 1},
+        {name: "Valera", id: 2},
+        {name: "Sasha", id: 3},
+        {name: "Sveta", id: 4},
+    ]
+
+    const DialogsElements = DialogsData.map(el => <Dialog key={el.id} title={el.name} id={el.id}/>)
+
+    const MessagesData: MessagesDatatype = [
+        {message: "Yo", id: 1},
+        {message: "hi!", id: 2},
+        {message: "How are you&", id: 3},
+    ]
+    const MessagesElements = MessagesData.map(el => <Message key={el.id} message={el.message}/>)
 
     return (
         <div className={s.dialogsWrapper}>
             <div className={s.dialogs}>
-                <Dialog title="Dima" id={1}/>
+
+                {DialogsElements}
+
+                {/*<Dialog title="Dima" id={1}/>
                 <Dialog title="Valera" id={2}/>
                 <Dialog title="Sasha" id={3}/>
-                <Dialog title="Sveta" id={4}/>
+                <Dialog title="Sveta" id={4}/>*/}
             </div>
             <div className={s.messages}>
-                <Message message="Yo"/>
+                {/*<Message message="Yo"/>
                 <Message message="hi!"/>
-                <Message message="How are you&"/>
+                <Message message="How are you&"/>*/}
+
+                {MessagesElements}
             </div>
         </div>
     );
--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -3,16 +3,26 @@ import s from './Posts.module.css'
 import {Post} from "./Post/Post";
 import {MyPost} from "./MyPost";
 
-
+type PostType = {
+    messageText: string
+    likesNumber: number
+}
+type PostsDatatype = PostType[]
 export const Posts = () => {
+
+    const PostsData: PostsDatatype = [
+        {messageText: "It's my first message", likesNumber: 16},
+        {messageText: "It's second message!", likesNumber: 4},
+
+    ]
+
+    const PostsElements = PostsData.map((post,index)=> <Post key={index} messageText={post.messageText} likesNumber={post.likesNumber}/>)
     return (
         <div className={s.profile}>
             <MyPost/>
 
             <div className={s.messages}>
-                <Post messageText={"It's my first message"} likesNumber={16}/>
-                <Post messageText={"It's second message"} likesNumber={4}/>
-
+                { PostsElements }
             </div>
         </div>
 
