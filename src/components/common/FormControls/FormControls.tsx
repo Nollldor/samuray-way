f27cb3f4715136d7c3db9690516efe59f044fdcf
Arthur Ishmiev <artur.ishmiev@gmail.com>
2023-02-14 15:50:14 +0300

    FormControl refactor

--- src/components/common/FormControls/FormControls.tsx
+++ src/components/common/FormControls/FormControls.tsx
@@ -1,17 +1,37 @@
-import {FC, TextareaHTMLAttributes} from "react";
+import {FC, HTMLAttributes, InputHTMLAttributes, ReactNode, TextareaHTMLAttributes} from "react";
 import styles from "./FormControl.module.css";
 
-type PropsType = {
-    input: TextareaHTMLAttributes<HTMLTextAreaElement>
+type FormControlPropsType = {
+    input: HTMLAttributes<HTMLElement>
     meta: any
+    children: React.ReactNode
 }
 
-export const Textarea: FC<PropsType> = ({input, meta, ...props}) => {
+export const FormControl: FC<FormControlPropsType> = ({input, children, meta, ...props}) => {
     const hasError = meta.touched && meta.error
     return <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
         <div>
-            <textarea {...input} {...props}/>
+            {children}
         </div>
         {hasError && <span>{meta.error}</span>}
     </div>
+}
+
+type ElementPropsType = {
+    input: HTMLAttributes<HTMLElement>
+    meta: any
+}
+
+export const Textarea: FC<ElementPropsType> = ({input, meta,...props}) => {
+    return <FormControl {...props} input={input} meta={meta}>
+        <textarea {...input} {...props}/>
+    </FormControl>
+
+}
+
+export const Input: FC<ElementPropsType> = ({input, meta,...props}) => {
+    return <FormControl {...props} input={input} meta={meta}>
+        <input {...input} {...props}/>
+    </FormControl>
+
 }
