f6087f9ae12b58f7580c386c56fd090e3f152202
Arthur Ishmiev <artur.ishmiev@gmail.com>
2023-02-21 16:50:00 +0300

    sync states with useEffect in ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, FC, useState} from "react";
+import {ChangeEvent, FC, useEffect, useState} from "react";
 
 type PropsType = {
     updateStatus: (status: string) => void
@@ -9,12 +9,16 @@ export const ProfileStatus: FC<PropsType> = ({updateStatus, status}) => {
     const [editMode, setEditMode] = useState<boolean>(false)
     const [title, setTitle] = useState<string>(status)
 
+    useEffect(() => {
+        setTitle(status)
+    }, [status])
+
     const onEditMode = () => {
         setEditMode(true)
     }
 
-    const onChangeStatus = (e:ChangeEvent<HTMLInputElement>) => {
-            setTitle(e.currentTarget.value)
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
     }
 
     const offEditMode = () => {
@@ -23,7 +27,7 @@ export const ProfileStatus: FC<PropsType> = ({updateStatus, status}) => {
     }
 
     return editMode ?
-        <input autoFocus={true} onBlur={offEditMode} placeholder={"Change your status"} onChange={onChangeStatus} value={title}/>
+        <input autoFocus={true} onBlur={offEditMode} placeholder={status} onChange={onChangeStatus} value={title}/>
         : <span onDoubleClick={onEditMode}>Status: {status} </span>
 
 }
