b1854acc2d0dc266a1197398e62b98c2c1906983
Arthur Ishmiev <artur.ishmiev@gmail.com>
2023-07-04 20:15:42 +0300

    fix: path "/" for ProfileContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, {lazy, useEffect} from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import {UsersContainer} from "./components/Users/UsersContainer";
 import {HeaderContainerConnect} from "./components/Header/HeaderContainer";
 import {Login} from "./components/Login/Login";
 import {useDispatch, useSelector} from "react-redux";
@@ -17,11 +16,12 @@ const DialogsContainer = lazy(() => import("./components/Dialogs/DialogsContaine
     .then(({DialogsContainer}) => ({default: DialogsContainer})));
 const ProfileContainer = lazy(() => import("./components/Profile/ProfileContainer")
     .then(({ProfileContainer}) => ({default: ProfileContainer})));
+const UsersContainer = lazy(() => import("./components/Users/UsersContainer")
+    .then(({UsersContainer}) => ({default: UsersContainer})));
 
 function App() {
     const dispatch = useDispatch()
     const isInitialized = useSelector<StateType, boolean>(state => state.app.initialized)
-
     useEffect(() => {
         dispatch(initializeAppTC())
     }, [])
@@ -36,15 +36,18 @@ function App() {
                 <Navbar/>
                 <div className={"app-wrapper-content"}>
                     <Routes>
-                        <Route path="/profile" element={
-                            <React.Suspense fallback={<div>Loading...</div>}><ProfileContainer/></React.Suspense>
-                        }>
-                            <Route path=":userId" element={<ProfileContainer/>}/>
-                        </Route>
+                        {["/profile", "/"].map((path, index) =>
+                            <Route path={path} element={<React.Suspense
+                                fallback={<div>Loading...</div>}><ProfileContainer/></React.Suspense>} key={index}>
+                                <Route path=":userId" element={<ProfileContainer/>}/>
+                            </Route>
+                        )}
                         <Route path="/messages" element={
                             <React.Suspense fallback={<div>Loading...</div>}><DialogsContainer/></React.Suspense>
                         }/>
-                        <Route path="/users" element={<UsersContainer/>}/>
+                        <Route path="/users" element={
+                            <React.Suspense fallback={<div>Loading...</div>}><UsersContainer/></React.Suspense>}
+                        />
                         <Route path="/news" element={<News/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/settings" element={<Settings/>}/>
